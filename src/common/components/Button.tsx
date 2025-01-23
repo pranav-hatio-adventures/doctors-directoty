88d199bbe9e18f27eeeda14e1aa3c531599a51a0
Pranav Madhu <pranav.hatio@gmail.com>
2025-01-23 10:42:56 +0530

    feat: Add Button component

---
+++ src/common/components/Button.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router";
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps {
+  label: string;
+  link_to?: string;
+  onClick?: () => void;
+  buttonClassName?: string;
+}
+const Button = (props: ButtonProps) => {
+  const className = twMerge(
+    "rounded-xl text-sm text-white py-2 px-3 bg-primary-color",
+    props.buttonClassName
+  );
+
+  if (props.link_to)
+    return (
+      <Link to={props.link_to} className={className}>
+        {props.label}
+      </Link>
+    );
+  return (
+    <button className={className} onClick={props.onClick}>
+      {props.label}
+    </button>
+  );
+};
+
+export default Button;
